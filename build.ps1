89c8a90a45ad1977ee072bce54ae1ed118a2b1dc
Alessandro Fabbri <rommacc@yahoo.it>
2018-03-19 12:02:15 +0100

    WARNING: CMAKE_BUILD_TYPE default value is 'Debug' on Win

--- build.ps1
+++ build.ps1
@@ -3,6 +3,6 @@
 mkdir build
 cd build
 #cmake -G "Visual Studio 15 2017 Win64" ..
-cmake -G "Ninja" ..
+cmake -G "Ninja" "-DCMAKE_BUILD_TYPE=Release" ..
 cmake --build . --target install #--config Release
 cd ..
